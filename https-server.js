a188a769d7559ad3ee04363dcf9facb20dee5b9e
Harshavardhan Gupta <hvgiitr@gmail.com>
2019-02-21 16:50:25 +0530

    Update https-server.js

--- https-server.js
+++ https-server.js
@@ -6,14 +6,17 @@ const options = {
   cert: fs.readFileSync('server.crt')
 };
 
+console.log("Echo server started.");
 https.createServer(options, function(request,response){    
     response.writeHead(200);
+	
     var responseData = "method: " + request.method + "\n" + "headers: " + JSON.stringify(request.headers) + "\n" + "url: " + request.url;
  
     var data = '';
     request.on('data', function(chunk) {
         data += chunk.toString();
     });
+	
     request.on('end', function() {
 	      responseData = responseData + "\n" + "body: " + data + "\n";
         console.log(responseData);
