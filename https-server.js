dfce7ef8662e3f054ff2b737675034db4532ab18
Harshavardhan Gupta <hvgiitr@gmail.com>
2019-02-21 16:20:57 +0530

    Create https-server.js
    Adding https echo server.

---
+++ https-server.js
@@ -0,0 +1,23 @@
+var https = require('https');
+const fs = require('fs');
+
+const options = {
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.crt')
+};
+
+https.createServer(options, function(request,response){    
+    response.writeHead(200);
+    var responseData = "method: " + request.method + "\n" + "headers: " + JSON.stringify(request.headers) + "\n" + "url: " + request.url;
+ 
+    var data = '';
+    request.on('data', function(chunk) {
+        data += chunk.toString();
+    });
+    request.on('end', function() {
+	      responseData = responseData + "\n" + "body: " + data + "\n";
+        console.log(responseData);
+        response.write(responseData);
+        response.end();
+    });
+}).listen(443);
